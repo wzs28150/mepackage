0a8bf84833b214e6c69136164588007cf70d8212
wzs28150 <wz666s@163.com>
2017-10-22 10:12:27 +0800

    add

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -1,36 +1,12 @@
-define([''], function() {
-  var initModule, scroll,resetsize;
+define(['scrollbar'], function(scrollbar) {
+  var initModule;
 
-  /**
-   * 滚动条
-   * @return {[type]} [description]
-   */
-  scroll = function(article) {
-
-    $(article).niceScroll({
-      cursorcolor: "#4284e2",
-      cursorwidth: "5px",
-
-
-      cursorfixedheight: false,
-      cursorminheight: 20,
-      enablekeyboard: true,
-      horizrailenabled: true,
-      bouncescroll: false,
-      smoothscroll: true,
-      zindex: 999
-    });
-
-  };
   initModule = function(article) {
-    scroll(article);
 
+    $(article).scrollbar();
   };
-  resetsize = function(article){
-      $(article).getNiceScroll(0).resize();
-  };
+
   return {
-    initModule: initModule,
-    resetsize: resetsize
+    initModule: initModule
   };
 })
